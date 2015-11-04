3dfebde39bc2ff78c43e6ca79a146e9307570951
usuario <usuario@10.22.143.15>
2015-11-04 10:29:31 +0000

    Cuarta Version

--- src/ejercicio4/Ejercicio4.java
+++ src/ejercicio4/Ejercicio4.java
@@ -1,54 +1,22 @@
 
 package ejercicio4;
 
-import java.io.BufferedInputStream;
-import java.io.BufferedOutputStream;
-import java.io.File;
-import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.util.Date;
+import java.util.ArrayList;
+
+
 
 
 public class Ejercicio4 {
  
     public static void main(String[] args) throws FileNotFoundException, IOException {
-       String name = "C:\\Users\\usuario\\Documents";
-        File file = new File (name);
-        
-        String [] filesarray = file.list();
-        
-        //for (Object files : filesarray) {
-         //   System.out.println(files);
-        //}
-        
-       // print (file.listFiles(), "");
-        
-        String from = "C:\\Users\\usuario\\Desktop\\Chrysanthemum.jpg";
-        String to = "C:\\Users\\usuario\\Desktop\\Chrysanthemum(1).jpg";
-        BufferedInputStream input = new BufferedInputStream(new FileInputStream(new File (from)));
-        BufferedOutputStream output = new BufferedOutputStream(new FileOutputStream(new File (to)));
-        byte [] buffer = new byte[1024] ;
-        Date date = new Date();
-        while(true){
-            int read = input.read(buffer);
-            if (read < 0) break;
-            output.write(buffer);
-        }
-        System.out.println(new Date().getTime()-date.getTime()+" msg");
-        input.close();
-        output.flush();
-        output.close();
-        
+       String name = "C:\\Users\\usuario\\Documents\\NetBeansProjects\\Ejercicio4\\DATA\\emails.txt";
+       ArrayList<String> listMail = MailList.read(name);
+       Histogram<String> histogram = MailHistogramBuilder.build(listMail);
+       HistogramDisplay histogramDisplay = new HistogramDisplay(histogram);
+       histogramDisplay.execute();
     }
     
-    private static void print(File[] listFiles, String ident){
-        if (listFiles == null) return;
-        for (File listFile : listFiles) {
-            System.out.println(ident+(listFile.isDirectory()? "+": "  ")+listFile.getName());
-            if (listFile.isFile() || listFile.isHidden()) continue;
-            print (listFile.listFiles(), ident + " ");
-        }
-    }
+    
 }
